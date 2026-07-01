4d34854fda28ba6eab8c1500add83e2abae7c3b9
Abel <abelgodinez99@gmail.com>
2026-07-01 04:16:43 +0100

    DatabaseRecorder y DatabaseStore

--- src/main/java/software/ulpgc/kata5/application/DatabaseStore.java
+++ src/main/java/software/ulpgc/kata5/application/DatabaseStore.java
@@ -22,25 +22,30 @@ public class DatabaseStore implements Store {
     public Stream<Movie> movies() throws IOException {
         try {
             return moviesIn(query());
-        }catch (SQLException e){
-            throw new RuntimeException();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private Stream<Movie> moviesIn(ResultSet resultSet) {
-        return Stream.generate(() -> moviesIn(resultSet).takeWhile(Objects::nonNull));
+    private Stream<Movie> moviesIn(ResultSet resultSet) throws SQLException {
+        return Stream.generate(() -> {
+            try {
+                return movieIn(resultSet);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }).takeWhile(Objects::nonNull);
     }
 
     private ResultSet query() throws SQLException {
-        return connection.createStatement().executeQuery("SELECT *" + "FROM MOVIES");
-
+        return connection.createStatement().executeQuery("SELECT * FROM MOVIES");
     }
 
     private Movie movieIn(ResultSet resultSet) throws SQLException {
         return resultSet.next() ? readFrom(resultSet) : null;
     }
 
-    private static Movie readFrom(ResultSet resultset) throws SQLException{
-        return new Movie(resultset.getString(1), resultset.getInt(2), resultset.getInt(3));
+    private static Movie readFrom(ResultSet resultSet) throws SQLException {
+        return new Movie(resultSet.getString(1), resultSet.getInt(2), resultSet.getInt(3));
     }
 }
